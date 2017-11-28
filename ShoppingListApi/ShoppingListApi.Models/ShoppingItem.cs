e61e234a5949d6ee28b30f09194f62c3ec40f9b7
Uffe <uffe_93@outlook.com>
2017-11-28 15:17:45 +0100

    First commit with new Api Solution on seperate branch

---
+++ ShoppingListApi/ShoppingListApi.Models/ShoppingItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingListApi.Models
+{
+    public class ShoppingItem
+    {
+        public ShoppingItem()
+        {
+            ShoppingLists = new List<ShoppingList>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsFavorite { get; set; }
+
+        public bool IsBought { get; set; }
+
+        public ICollection<ShoppingList> ShoppingLists { get; set; }
+    }
+}
---
+++ ShoppingListApi/ShoppingListApi.Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingListApi.Models
+{
+    public class User
+    {
+        public User()
+        {
+            ShoppingLists = new List<ShoppingList>();
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Mail { get; set; }
+
+        public ICollection<ShoppingList> ShoppingLists { get; set; }
+    }
+}
