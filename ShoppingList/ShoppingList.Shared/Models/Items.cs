03624fe65e4864ac237140047780c43aa5ef8a9a
Philip ryström <prys45@hotmail.com>
2017-11-28 15:09:14 +0100

    Added users/items

---
+++ ShoppingList/ShoppingList.Shared/Models/Items.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.Shared.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
