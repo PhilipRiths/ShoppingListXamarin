24858cf8e557cf98b30d2ce144cc84be9bb0bfc3
Alem Putes <alem.iths@gmail.com>
2017-11-28 20:23:46 +0100

    Update App to Prism application instead, added Prism Autofac, updated Andriod/iOS applications to reflect the change.

---
+++ ShoppingList/ShoppingList.Shared/ViewModels/ShoppingListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace ShoppingList.Shared.ViewModels
+{
+    public class ShoppingListViewModel : BindableBase
+    {
+        public ShoppingListViewModel()
+        {
+            AddShoppingListCommand = new DelegateCommand(AddShoppingListExecute);
+        }
+
+        public ICommand AddShoppingListCommand { get; }
+
+        private void AddShoppingListExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
