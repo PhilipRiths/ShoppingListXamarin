a8168f6aa5d9a85b069251d0252535fbba86b783
Uffe_O <uffe_93@outlook.com>
2017-11-29 15:40:22 +0100

    Added ShoppingListController and ShoppingListDbContext

---
+++ ShoppingListApi/ShoppingListApi/Data/ShoppingListDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingListApi.Models;
+
+namespace ShoppingListApi.Data
+{
+    public class ShoppingListDbContext : DbContext
+    {
+        public ShoppingListDbContext()
+        {
+        }
+
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+    }
+}
