7f602b31c3ca9557b26d3e171a04a2c3d4f61037
Philip ryström <prys45@hotmail.com>
2018-01-11 17:56:21 +0100

    Added buttons for edit/delete

--- ShoppingList/ShoppingList.Android/Resources/Resource.Designer.cs
+++ ShoppingList/ShoppingList.Android/Resources/Resource.Designer.cs
@@ -2544,26 +2544,26 @@ namespace ShoppingList.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02015a
-			public const int avd_hide_password_1 = 2130837850;
-			
 			// aapt resource value: 0x7f02015b
-			public const int avd_hide_password_2 = 2130837851;
+			public const int avd_hide_password_1 = 2130837851;
 			
 			// aapt resource value: 0x7f02015c
-			public const int avd_hide_password_3 = 2130837852;
+			public const int avd_hide_password_2 = 2130837852;
+			
+			// aapt resource value: 0x7f02015d
+			public const int avd_hide_password_3 = 2130837853;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02015d
-			public const int avd_show_password_1 = 2130837853;
-			
 			// aapt resource value: 0x7f02015e
-			public const int avd_show_password_2 = 2130837854;
+			public const int avd_show_password_1 = 2130837854;
 			
 			// aapt resource value: 0x7f02015f
-			public const int avd_show_password_3 = 2130837855;
+			public const int avd_show_password_2 = 2130837855;
+			
+			// aapt resource value: 0x7f020160
+			public const int avd_show_password_3 = 2130837856;
 			
 			// aapt resource value: 0x7f020056
 			public const int basket_fill = 2130837590;
@@ -3321,11 +3321,11 @@ namespace ShoppingList.Droid
 			// aapt resource value: 0x7f020151
 			public const int notification_icon_background = 2130837841;
 			
-			// aapt resource value: 0x7f020158
-			public const int notification_template_icon_bg = 2130837848;
-			
 			// aapt resource value: 0x7f020159
-			public const int notification_template_icon_low_bg = 2130837849;
+			public const int notification_template_icon_bg = 2130837849;
+			
+			// aapt resource value: 0x7f02015a
+			public const int notification_template_icon_low_bg = 2130837850;
 			
 			// aapt resource value: 0x7f020152
 			public const int notification_tile_bg = 2130837842;
@@ -3334,16 +3334,19 @@ namespace ShoppingList.Droid
 			public const int notify_panel_notification_icon_bg = 2130837843;
 			
 			// aapt resource value: 0x7f020154
-			public const int roundedbg = 2130837844;
+			public const int pencil = 2130837844;
 			
 			// aapt resource value: 0x7f020155
-			public const int roundedbgdark = 2130837845;
+			public const int roundedbg = 2130837845;
 			
 			// aapt resource value: 0x7f020156
-			public const int tooltip_frame_dark = 2130837846;
+			public const int roundedbgdark = 2130837846;
 			
 			// aapt resource value: 0x7f020157
-			public const int tooltip_frame_light = 2130837847;
+			public const int tooltip_frame_dark = 2130837847;
+			
+			// aapt resource value: 0x7f020158
+			public const int tooltip_frame_light = 2130837848;
 			
 			static Drawable()
 			{
