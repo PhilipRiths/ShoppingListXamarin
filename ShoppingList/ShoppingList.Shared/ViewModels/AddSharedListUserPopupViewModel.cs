28e31e84b7cf0e80a68d36444da08d2c4ee0d738
DESKTOP-RC0V4EJ\Alem Putes <alem.putes@gmail.com>
2018-01-13 19:56:38 +0100

    Cancel popup

--- ShoppingList/ShoppingList.Shared/ViewModels/AddSharedListUserPopupViewModel.cs
+++ ShoppingList/ShoppingList.Shared/ViewModels/AddSharedListUserPopupViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace ShoppingList.Shared.ViewModels
+﻿using System.Threading.Tasks;
+
+using Prism.Commands;
+using Prism.Navigation;
+
+using ShoppingList.Shared.Helpers;
+
+namespace ShoppingList.Shared.ViewModels
 {
     public class AddSharedListUserPopupViewModel : BaseViewModel
     {
-        public string TestLabel { get; set; } = "Moo";
+        private readonly INavigationService _navigationService;
+
+        public AddSharedListUserPopupViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            CancelCommand = new DelegateCommand(() => _navigationService.GoBackAsync());
+        }
+
+        public DelegateCommand CancelCommand { get; }
+
+        public string Email { get; set; }
     }
 }
